4373f0b9ff46ee5b2bfb80dab701873b1ad988d0
Dane Schmidt <daneschmidt@gmail.com>
2019-12-14 10:33:09 -0600

    db created, get is setup and I can GET data back in Postman at /get_feedback

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "feedback" ORDER BY "id" ASC;';
+    pool.query(queryText).then(result => {
+      res.send(result.rows);
+    })
+    .catch(error => {
+      console.log('error getting books', error);
+      res.sendStatus(500);
+    });
+  });
+
+  module.exports = router;
