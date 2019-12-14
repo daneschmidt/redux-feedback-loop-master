10ccb95969ac778981123055c1e7d2b56ae6dfe8
Dane Schmidt <daneschmidt@gmail.com>
2019-12-14 12:19:48 -0600

    setting up pages components

---
+++ src/components/pages/Feeling/Feeling.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Feeling extends Component {
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
+export default Feeling;
---
+++ src/components/pages/Supported/Supported.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Supported extends Component {
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
+export default Supported;
